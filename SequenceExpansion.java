b7e69fbc304778e4df5e3d6627728fc0e7747bf4
tagorehyd <51019076+tagorehyd@users.noreply.github.com>
2020-06-22 13:34:41 +0530

    Update SequenceExpansion.java
    modifications

--- SequenceExpansion.java
+++ SequenceExpansion.java
@@ -1,17 +1,17 @@
 import  java.util.*;
-class Expansion {
+public class Expansion {
     public static class Capsule {
-        Integer multiplier;
+        Long multiplier;
         String data;
         public Capsule() { }
-        public Capsule(Integer multiplier, String data) {
+        public Capsule(Long multiplier, String data) {
             this.multiplier = multiplier;
             this.data = data;
         }
-        public Integer getMultiplier() {
+        public Long getMultiplier() {
             return multiplier;
         }
-        public void setMultiplier(Integer multiplier) {
+        public void setMultiplier(Long multiplier) {
             this.multiplier = multiplier;
         }
         public String getData() {
@@ -25,31 +25,45 @@ class Expansion {
                     ", data='" + data + '\'' +'}';
         }
     }
+    static String testCase="5 " +
+            "university college department student rupees " +
+            "university  320 college " +
+            "college  7 department " +
+            "department  2400 student " +
+            "student  80000 rupees";
+    static String testCase2="5 " +
+            "university college department student rupees " +
+            "university 430080000000 rupees " +
+            "college 1344000000 rupees " +
+            "department 192000000 rupees " +
+            "student 80000 rupees";
     static Scanner z=new Scanner (System.in);
     public static void main(String[] args) {
         List<String> orderSequence=new ArrayList<>();
         Map<String,Capsule> map=new HashMap<>();
         for(int i=z.nextInt();i>0;i--){
-            orderSequence.add(z.next().toUpperCase());
+            orderSequence.add(z.next() );
         }
         for(int i=0;i<orderSequence.size()-1;i++){
-            String lhs=z.next().toUpperCase();
-            Integer multiplier=z.nextInt();
-            String rhs=z.next().toUpperCase();
+            String lhs=z.next() ;
+            Long multiplier=z.nextLong();
+            String rhs=z.next() ;
             map.put(lhs,new Capsule(multiplier,rhs ) );
-            if(i==orderSequence.size()-1) map.put(rhs,new Capsule(1,null));
+            if(i==orderSequence.size()-2) map.put(rhs,new Capsule(1L,null));
         }
-        boolean isLinear=!orderSequence.get(orderSequence.size()-1).equalsIgnoreCase(orderSequence.get(0));
+
+        boolean isLinear= !map.get(orderSequence.get(0)).getData().equalsIgnoreCase(map.get(orderSequence.get(orderSequence.size()-2)).getData());
+
         System.out.println(expand(orderSequence,map,isLinear));
     }
     static String expand(List<String> seq,Map<String ,Capsule> map,boolean linearInput){
         if(linearInput)
         {
-            return expansionHelperForLinearInput(seq,new String(),map,1);
+            return expansionHelperForLinearInput(seq,new String(),map,1L);
         }
-        return expansionHelperForNonLinearInput(seq,new String(),map,1);
+        return expansionHelperForNonLinearInput(seq,new String(),map,1L);
     }
-    static String expansionHelperForLinearInput(List<String> seq, String dat, Map<String ,Capsule> map, int mul){
+    static String expansionHelperForLinearInput(List<String> seq, String dat, Map<String ,Capsule> map, Long mul){
         for(int i=0;i<seq.size()-1;i++){
             mul=mul*map.get(seq.get(i)).getMultiplier();
             dat+=seq.get(i)+
@@ -58,16 +72,16 @@ class Expansion {
         }
         return dat.trim()+seq.get(seq.size()-1);
     }
-    static String expansionHelperForNonLinearInput(List<String> seq, String dat, Map<String ,Capsule> map, Integer mul){
-       map=regularizeData(seq,map);
+    static String expansionHelperForNonLinearInput(List<String> seq, String dat, Map<String ,Capsule> map, Long mul){
+        map=regularizeData(seq,map);
         return expansionHelperForLinearInput(seq,dat,map,mul);
     }
     static Map<String,Capsule> regularizeData(List<String> seq, Map<String ,Capsule> map){
         Map<String,Capsule> newMap=new HashMap<>();
         for(int i=0;i<seq.size()-1;i++){
-            int upper=map.get(seq.get(i)).getMultiplier(),
-            lower=map.get(seq.get(i+1)).getMultiplier();
-            int mul=upper/lower;
+            Long upper=map.get(seq.get(i)).getMultiplier(),
+                    lower=map.get(seq.get(i+1)).getMultiplier();
+            Long mul=upper/lower;
             newMap.put(seq.get(i), new Capsule(mul,seq.get(i+1)) );
         }
         return newMap;
